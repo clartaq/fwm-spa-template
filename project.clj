2329c4cac718f37ce2d17cc5916f3071e477b818
clartaq <david@axaluna.com>
2020-08-06 13:17:30 -0400

    Added references to github repository.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fwm-spa/lein-template "0.1.1-RELEASE"
   :description "A Leiningen template for figwheel-main and Reagent"
-  :url "https://github.com/clartaq/fwm-spa-template"
+  :url "https://github.com/clartaq/fwm-spa-templateq"
   :license {:name "Eclipse Public License 2.0 or later"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
